4003057c1499a961cee2193423b8497e1467233f
minsgy <alstjrchl97@likelion.org>
2023-11-25 00:12:03 +0900

    init: project start setting

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Frame } from "./components/Frame";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useMemo } from "react";
+import { QuizListPage } from "./pages/QuizListPage";
+
+function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route element={<Frame />}>
+          <Route path="/quiz" element={<QuizListPage />} />
+          <Route path="/quiz/create" element={<div>create</div>} />
+          <Route path="/quiz/:id" element={<div>detail</div>} />
+          <Route path="*" element={<Navigate to="/quiz" replace />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
+  );
+}
+
+export default App;
